d650fd804be9f027e0481c6e6994794e0fbaf6a7
kartikver15gr8 <vermakartikey786@gmail.com>
2024-09-29 19:07:42 +0530

    modified actions.json

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -2,22 +2,29 @@ import { createActionHeaders, type ActionsJson } from "@solana/actions";
 
 export const GET = async () => {
   const payload: ActionsJson = {
+    // rules: [
+    //   {
+    //     pathPattern: "/product/TOP_100_VCs_LIST",
+    //     apiPath: "/api/actions/buy/**",
+    //   },
+    //   {
+    //     pathPattern: "/product/**",
+    //     apiPath: "https://hypermarket.vercel.app/api/actions/buy/**",
+    //   },
+    //   {
+    //     pathPattern: "/product/**",
+    //     apiPath: "https://sendit.markets/api/actions/blinks/**",
+    //   },
+    //   {
+    //     pathPattern: "/product/**",
+    //     apiPath: "https://hypermarket.vercel.app/blinks/product/**",
+    //   },
+    // ],
+
     rules: [
       {
-        pathPattern: "/product/TOP_100_VCs_LIST",
-        apiPath: "/api/actions/buy/**",
-      },
-      {
         pathPattern: "/product/**",
-        apiPath: "https://hypermarket.vercel.app/api/actions/buy/**",
-      },
-      {
-        pathPattern: "/product/**",
-        apiPath: "https://sendit.markets/api/actions/blinks/**",
-      },
-      {
-        pathPattern: "/product/**",
-        apiPath: "https://hypermarket.vercel.app/blinks/product/**",
+        apiPath: "https://api.sendit.markets/api/v1/blinks/product/**",
       },
     ],
   };
