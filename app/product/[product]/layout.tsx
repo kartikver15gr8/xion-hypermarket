0c9e47319e7ed42264d249cc6d8ed6a63148ea72
kartikver15gr8 <vermakartikey786@gmail.com>
2024-09-29 22:04:33 +0530

    added dynamic Opengraph in products/

---
+++ app/product/[product]/layout.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+import { ProductInterface } from "@/lib/models";
+
+async function getProduct(productId: string): Promise<ProductInterface> {
+  console.log(`Fetching product with ID: ${productId}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_SWAGGER_URL}/fetch/products?product_id=${productId}`
+  );
+  const data = await response.json();
+  console.log("Fetched product data:", data[0]);
+  return data[0];
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { product: string };
+}): Promise<Metadata> {
+  console.log("Generating metadata for product:", params.product);
+  const product = await getProduct(params.product);
+
+  return {
+    title: product.Name,
+    description: product.Description,
+    openGraph: {
+      title: product.Name,
+      description: product.Description,
+      images: [`/product/${params.product}/opengraph-image`],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: product.Name,
+      description: product.Description,
+      images: [`/product/${params.product}/opengraph-image`],
+    },
+  };
+}
+
+export default function ProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
